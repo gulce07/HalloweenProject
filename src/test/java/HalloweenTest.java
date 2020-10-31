3cbaa298706aff2edfd613fe73d5a94fdd8b27ed
GulCe <gulay.serbet@gmail.com>
2020-10-31 16:48:30 -0400

    updating the Halloween test from magic branch

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,10 +1,14 @@
 public class HalloweenTest {
+    // message from magic branch
+
     // I'm about to give everyone candy for halloween
     // Hello git
     // adding new lines
 
     // line 1
     // line 2
+
+
 
 
 
