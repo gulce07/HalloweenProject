eb5c9f4c428ea2fa26f88797a1c210a5b6155476
GulCe <gulay.serbet@gmail.com>
2020-10-31 14:37:03 -0400

    commit new folder2

--- src/main/java/FeatureACode.java
+++ src/main/java/FeatureACode.java
@@ -1,2 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class FeatureACode {
+
+    public static void main(String[] args) {
+        System.out.println("awesome feature");
+    }
 }
